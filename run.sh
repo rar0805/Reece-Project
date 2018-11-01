a2ff2ec3bba7d5370c00f83bd883421b8f57f072
Reece Rhodes <rar0805@westminstercollege.edugit config --global user.email rar0805@westminstercollege.edu>
2018-11-01 16:38:42 -0600

    Commited run.sh

---
+++ run.sh
@@ -0,0 +1,27 @@
+mv index.html /new
+first='
+<html>
+<head>
+<title>
+WELCOME TO MY POORLY DONE PROJECT!!!!!!!!!
+</title>
+</head>
+<body>
+<h1>
+I have probably spent hours and hours on this by the time you see it...
+SO PREPARE TO BE DISSAPOINTED!
+FOLLOW THE LINK BELOW TO BE EVEN MORE IMPRESSED!!!
+</h1>
+<a href="A.html"</a>
+</body>
+</html>
+'
+second='
+<html>
+<body>
+See? Was this not Amazing?
+Yeah I thought so too.
+</body>
+</html>'
+echo $first > index.html
+echo $second > A.html
